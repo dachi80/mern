583f0d50abb661e0465e58ef195c172c2753a11e
David Chikhradze <d.chikhradze@gmail.com>
2022-03-30 18:20:33 +0400

    create login

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,84 +1,87 @@
-import React, {useEffect, useState} from 'react'
-import {useHttp} from '../hooks/http.hook'
-import {useMessage} from "../hooks/message.hook";
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
 
 export const AuthPage = () => {
-    const message = useMessage()
-    const {loading, request, error, clearError} = useHttp()
-    const [form, setForm] = useState({
-        email: '', password: ''
-    })
+  const auth = useContext(AuthContext)
+  const message = useMessage()
+  const { loading, request, error, clearError } = useHttp()
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  })
 
-    useEffect(() => {
-        message(error)
-        clearError()
-    }, [error, message, clearError])
+  useEffect(() => {
+    message(error)
+    clearError()
+  }, [error, message, clearError])
 
-    const changeHandler = event => {
-        setForm ({ ...form, [event.target.name]: event.target.value })
-    }
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
 
-    const registerHandler = async () => {
-        try {
-            const data = await request('/api/auth/register', 'POST', {...form})
-            message(data.message)
-        } catch (e) {}
-    }
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', { ...form })
+      message(data.message)
+    } catch (e) {}
+  }
 
-    const loginHandler = async () => {
-        try {
-            const data = await request('/api/auth/login', 'POST', {...form})
-            message(data.message)
-        } catch (e) {}
-    }
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', { ...form })
+      auth.login(data.token, data.userId)
+    } catch (e) {}
+  }
 
-    return (
-        <div className="row">
-            <div className="col s6 offset-s3 center">
-                <h3>სისტემაში შესვლა</h3>
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">ავტორიზაცია</span>
-                        <div>
-                            <div className="input-field">
-                                <input
-                                    id="email"
-                                    type="text"
-                                    name="email"
-                                    onChange={changeHandler}
-                                />
-                                <label htmlFor="email">ელ. ფოსტა</label>
-                            </div>
-                            <div className="input-field">
-                                <input
-                                    id="password"
-                                    type="password"
-                                    name="password"
-                                    onChange={changeHandler}
-                                />
-                                <label htmlFor="password">პაროლი</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-action center">
-                        <button
-                            className="btn yellow darken-4"
-                            style={{marginRight: 10}}
-                            disabled={loading}
-                            onClick={loginHandler}
-                        >
-                            შესვლა
-                        </button>
-                        <button
-                            className="btn grey lighten-1 black-text"
-                            disabled={loading}
-                            onClick={registerHandler}
-                        >
-                            რეგისტრაცია
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3 center">
+        <h3>სისტემაში შესვლა</h3>
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">ავტორიზაცია</span>
+            <div>
+              <div className="input-field">
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">ელ. ფოსტა</label>
+              </div>
+              <div className="input-field">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="password">პაროლი</label>
+              </div>
             </div>
+          </div>
+          <div className="card-action center">
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+              onClick={loginHandler}
+            >
+              შესვლა
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              disabled={loading}
+              onClick={registerHandler}
+            >
+              რეგისტრაცია
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import {LinksPage} from './pages/LinksPage'
-import {CreatePage} from './pages/CreatePage'
-import {DetailPage} from './pages/DetailPage'
-import {AuthPage} from './pages/AuthPage'
-import {ErrorPage} from "./pages/ErrorPage";
+import { Routes, Route } from 'react-router-dom'
+import { LinksPage } from './pages/LinksPage'
+import { CreatePage } from './pages/CreatePage'
+import { DetailPage } from './pages/DetailPage'
+import { AuthPage } from './pages/AuthPage'
+import { ErrorPage } from './pages/ErrorPage'
 
-export const useRoutes = isAuthenticated => {
-    if (isAuthenticated) {
-        return (
-            <Routes>
-                <Route path="/links" element={<LinksPage />}/>
-                <Route path="/create" element={<CreatePage />}/>
-                <Route path="/detail/:id" element={<DetailPage />}/>
-                <Route path="*" element={<ErrorPage />}/>
-            </Routes>
-        )
-    }
+export const useRoutes = (isAuthenticated) => {
+  if (isAuthenticated) {
     return (
-        <Routes>
-            <Route path="*" element={<AuthPage />}/>
-        </Routes>
+      <Routes>
+        <Route path="/links" element={<LinksPage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     )
+  }
+  return (
+    <Routes>
+      <Route path="*" element={<AuthPage />} />
+    </Routes>
+  )
 }
